06940e5e40ba46e21cbe81387ab43dda59b55c43
Nelly <nellyobijiaku@gmail.com>
2024-04-05 10:31:51 +0100

    Added the js and css to the dictionary page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> </header>
-      Hello
+      <div className="container">
+      <header className="App-header"> 
+      
+      </header>
+      <main>
+        <Dictionary /> 
+      </main>
+      <footer className="App-footer">
+
+       <small>Coded by <a href= "github.com/nellyobi"> Nelly Obi </a> and is <a href="github.com/nellyobi/dictionary-project"> open-sourced </a>  on Github.</small> 
+      </footer>
+      </div>
     </div>
   );
 }
